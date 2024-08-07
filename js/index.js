904f0146325571e975aff09f5341cbb0f1058d4a
anahelenaa <anahelenaesperanca@gmail.com>
2024-08-07 20:20:39 -0300

    adição das informações no footer e correção no layout do menu

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 const botaoAbrir = document.getElementById("menu-botao");
 const botaoFechar = document.getElementById("menu-botao-fechar");
+var cabecalho = document.getElementById("cabecalho");
+var corpo = document.getElementById("corpo");
+
+window.onscroll = function () {
+    aoScrollar();
+};
+
 
 botaoAbrir.addEventListener('click', abrirMenu);
 botaoFechar.addEventListener('click', fecharMenu);
@@ -10,7 +17,7 @@ function abrirMenu() {
 
     document.getElementById("corpo").classList.add("corpo-menu-aberto");
     document.getElementById("corpo").classList.remove("corpo-menu-fechado");
-}
+};
 
 function fecharMenu() {
     document.getElementById("mySidebar").classList.remove("menu-aberto");
@@ -18,4 +25,17 @@ function fecharMenu() {
 
     document.getElementById("corpo").classList.remove("corpo-menu-aberto");
     document.getElementById("corpo").classList.add("corpo-menu-fechado");
-}
+};
+
+function aoScrollar() {
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    if (scrollTop >= 15) {
+      cabecalho.classList.add("sticky");
+      corpo.classList.add("conteudo");
+  
+    } else {
+      cabecalho.classList.remove("sticky");
+      cabecalho.classList.remove("sticky_remove");
+      corpo.classList.remove("conteudo");
+    }
+};
