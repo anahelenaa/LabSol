b7939b75413226ef8f41499c9cc294fc4fc30658
BrunoHidekiUemura <brunouemura22@gmail.com>
2024-08-25 20:32:32 -0300

    add js

--- js/index.js
+++ js/index.js
@@ -39,3 +39,23 @@ function aoScrollar() {
       corpo.classList.remove("conteudo");
     }
 };
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval(function(){
+  nextImage();
+},5000)
+
+function nextImage(){
+  count++;
+
+  if(count > 4){
+    count = 1;
+  }
+  document.getElementById("radio"+count).checked = true;
+};
+
+
+
+
